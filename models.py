15b71d090a107637118c72364475dbf9dacd8334
Luke O'Drobinak <luke.d.odrobinak@gmail.com>
2025-11-28 15:47:39 -0700

    Including all packages and requirements file

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-from datetime import datetime
+# from datetime import datetime
 
 class ItemCreate(BaseModel):
     name: str = Field(..., description="Name of the item")
